ba0d1810af7de6d4c6937ecb434dd3683b4fef25
SIARHEI SAVERCHANKA <saverchenko2002@gmail.com>
2021-12-30 18:26:39 +0300

    add validation

--- force-app/main/default/aura/QuickContact/QuickContactController.js
+++ force-app/main/default/aura/QuickContact/QuickContactController.js
@@ -1,22 +1,34 @@
 ({
   doSave: function (component, event, helper) {
-    var action = component.get("c.createContact");
-    action.setParams({
-      contact: component.get("v.createContact"),
-      accountId: component.get("v.accountId")
-    });
-    action.setCallback(
-      this,
-      function (response) {
-        alert(response.getState());
-        if (state === "SUCCESS" || state === "DRAFT") {
-          var responseValue = response.getReturnValue();
-        } else if (state === "INCOMPLETE") {
-        } else if (state === "ERROR") {
-        }
-      },
-      "ALL"
-    );
-    $A.enqueueAction(action);
+    var validContact = component.find("contactForm").reduce(function (validSoFar, inputCmp) {
+      //   inputCmp.showHelpMessageIfInvalid();
+      console.log(inputCmp.get("v.validity").valid);
+      return validSoFar && inputCmp.get("v.validity").valid;
+    }, true);
+
+    console.log("res " + validContact);
+
+    // var action = component.get("c.createContact");
+    // action.setParams({
+    //   contact: component.get("v.createContact"),
+    //   accountId: component.get("v.accountId")
+    // });
+    // action.setCallback(
+    //   this,
+    //   function (response) {
+    //     var state = response.getState();
+    //     if (state === "SUCCESS" || state === "DRAFT") {
+    //       var responseValue = response.getReturnValue();
+    //     } else if (state === "INCOMPLETE") {
+    //     } else if (state === "ERROR") {
+    //       console.log("////");
+    //       var errors = response.getError();
+    //       console.log(errors);
+    //       console.log(JSON.stringify(errors));
+    //     }
+    //   },
+    //   "ALL"
+    // );
+    // $A.enqueueAction(action);
   }
 });
